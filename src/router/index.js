253f2c1b34003200f7fa9e49fd289ec5919a69ed
tsungilin <tsungilin@bitdigitaltech.com>
2022-01-15 10:40:22 +0800

    設定主頁相關路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,16 @@ import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
 import SignIn from '../views/SignIn.vue'
 import SignUp from '../views/SignUp.vue'
+import Restaurant from '../views/Restaurants.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -16,6 +22,26 @@ const routes = [
     path: '/signup',
     name: 'sign-up',
     component: SignUp
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurant
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-top',
+    component: () => import('../views/RestaurantsTop')
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop.vue')
   },
   {
     path: '*',

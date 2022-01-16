80a2b56700588d105c3653b2f4145aaef6caef4d
tsungilin <tsungilin@bitdigitaltech.com>
2022-01-16 11:13:24 +0800

    add comments and restaurantsFeeds components & moment module

---
+++ src/components/NewestRestaurants.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      最新餐廳
+    </div>
+    <div class="card-body">
+      <div v-for="restaurant in restaurants" :key="restaurant.id">
+        <h4>
+          <a href="#">{{restaurant.name}}</a>
+          <small>{{restaurant.Category ? restaurant.Category.name : '未分類'}}</small>
+        </h4>
+        <p>{{restaurant.description}}</p>
+        {{restaurant.createdAt | fromNow}}
+        <hr>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+    props: {
+        restaurants: {
+            type: Array,
+            require: true
+        }
+    },
+    filters: {
+        fromNow(value) {
+            if(!value) {
+                return '--'
+            }
+            return moment(value).fromNow()
+        }
+    }
+}
+</script>
+
+<style scoped>
+small {
+  font-size: 10px;
+}
+</style>

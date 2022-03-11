84119490df239f08ac4708e36acf1098a9c69720
tsungilin <mt5718214@gmail.com>
2022-03-11 19:45:11 +0800

    完成新增餐廳功能

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container py-5">
+    <!-- 餐廳表單 AdminRestaurantForm -->
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from "../components/AdminRestaurantForm.vue";
+export default {
+  name: "AdminRestaurantNew",
+  components: {
+    AdminRestaurantForm,
+  },
+  methods: {
+    handleAfterSubmit(formData) {
+      // TODO: 透過 API 將表單資料送到伺服器
+      for (let [name, value] of formData) {
+        console.log(name + ": " + value);
+      }
+    },
+  },
+};
+</script>

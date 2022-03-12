7031ae8a3c664519309e3a8746dac821c37fe15a
tsungilin <mt5718214@gmail.com>
2022-03-12 16:46:22 +0800

    完成餐廳類別頁面功能

---
+++ src/views/AdminCategories.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="container py-5">
+    <!-- 後台導覽頁籤 AdminNav -->
+    <AdminNav />
+
+    <form class="my-4">
+      <div class="form-row">
+        <div class="col-auto">
+          <input
+            v-model="newCategoryName"
+            type="text"
+            class="form-control"
+            placeholder="新增餐廳類別..."
+          />
+        </div>
+        <div class="col-auto">
+          <button
+            type="button"
+            class="btn btn-primary"
+            @click.stop.prevent="createCategory"
+          >
+            新增
+          </button>
+        </div>
+      </div>
+    </form>
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col" width="60">#</th>
+          <th scope="col">Category Name</th>
+          <th scope="col" width="210">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="category in categories" :key="category.id">
+          <th scope="row">
+            {{ category.id }}
+          </th>
+          <td class="position-relative">
+            <div v-show="!category.isEditing" class="category-name">
+              {{ category.name }}
+            </div>
+            <input
+              v-show="category.isEditing"
+              v-model="category.name"
+              type="text"
+              class="form-control"
+            />
+            <span
+              v-show="category.isEditing"
+              class="cancel"
+              @click="handleCancel(category.id)"
+            >
+              ✕
+            </span>
+          </td>
+          <td class="d-flex justify-content-between">
+            <button
+              v-show="!category.isEditing"
+              type="button"
+              class="btn btn-link mr-2"
+              @click="toggleEditing(category.id)"
+            >
+              Edit
+            </button>
+            <button
+              v-show="category.isEditing"
+              type="button"
+              class="btn btn-link mr-2"
+              @click="updateCategory(category.id)"
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              class="btn btn-link mr-2"
+              @click.stop.prevent="deleteCategory(category.id)"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import AdminNav from "@/components/AdminNav";
+import { v4 as uuidv4 } from "uuid";
+const dummyData = {
+  categories: [
+    {
+      id: 1,
+      name: "中式料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z",
+    },
+    {
+      id: 2,
+      name: "日本料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z",
+    },
+    {
+      id: 3,
+      name: "義大利料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z",
+    },
+    {
+      id: 4,
+      name: "墨西哥料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z",
+    },
+  ],
+};
+
+export default {
+  name: "admin-categories",
+  components: {
+    AdminNav,
+  },
+  data() {
+    return {
+      categories: [],
+      newCategoryName: "",
+    };
+  },
+  created() {
+    this.fetchCategories();
+  },
+  methods: {
+    fetchCategories() {
+      this.categories = dummyData.categories.map((category) => ({
+        ...category,
+        isEditing: false,
+        nameCached: "",
+      }));
+    },
+    createCategory() {
+      this.categories.push({
+        id: uuidv4(),
+        name: this.newCategoryName,
+      });
+
+      this.newCategoryName = "";
+    },
+    deleteCategory(id) {
+      this.categories = this.categories.filter(
+        (category) => category.id !== id
+      );
+    },
+    updateCategory(categoryId) {
+      // TODO: 透過 API 去向伺服器更新餐廳類別名稱
+      this.toggleEditing(categoryId);
+    },
+    handleCancel(categoryId) {
+      this.categories.forEach((category) => {
+        if (category.id === categoryId) {
+          category.name = category.nameCached;
+        }
+      });
+
+      this.toggleEditing(categoryId);
+    },
+    toggleEditing(categoryId) {
+      this.categories.forEach((category) => {
+        if (category.id === categoryId) {
+          category.isEditing = !category.isEditing;
+          category.nameCached = category.name;
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.category-name {
+  padding: 0.375rem 0.75rem;
+  border: 1px solid transparent;
+  outline: 0;
+  cursor: auto;
+}
+
+.btn-link {
+  width: 62px;
+}
+
+.cancel {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #aaaaaa;
+  border-radius: 50%;
+  user-select: none;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>
